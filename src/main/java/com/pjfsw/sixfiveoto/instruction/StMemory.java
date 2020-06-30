b83a2bf62e016c8764ce88bf23092572adb58097
Johan Fransson <per.johan.fransson@gmail.com>
2020-06-30 20:06:37 +0200

    Completed STA/STX/STY

--- src/main/java/com/pjfsw/sixfiveoto/instruction/StMemory.java
+++ src/main/java/com/pjfsw/sixfiveoto/instruction/StMemory.java
@@ -2,18 +2,37 @@ package com.pjfsw.sixfiveoto.instruction;
 
 import java.util.function.Function;
 
-import com.pjfsw.sixfiveoto.Memory;
 import com.pjfsw.sixfiveoto.addressables.Peeker;
 import com.pjfsw.sixfiveoto.addressables.Poker;
 import com.pjfsw.sixfiveoto.registers.Registers;
 
 public enum StMemory implements Instruction {
-
+    // Absolute
     STA(Registers::a, AddressingMode.ABSOLUTE, 0x8D, "STA $%04X", 4, 2),
     STX(Registers::x, AddressingMode.ABSOLUTE, 0x8E, "STX $%04X", 4, 2),
     STY(Registers::y, AddressingMode.ABSOLUTE, 0x8C, "STY $%04X", 4, 2),
+
+    // Indexed
     STAX(Registers::a, AddressingMode.INDEXED_X, 0x9D, "STA $%04X,X", 5, 2),
-    STAY(Registers::a, AddressingMode.INDEXED_Y, 0x99, "STA $%04X,Y", 5, 2);
+    STAY(Registers::a, AddressingMode.INDEXED_Y, 0x99, "STA $%04X,Y", 5, 2),
+
+    // Zeropage
+    STAZ(Registers::a, AddressingMode.ZEROPAGE, 0x85, "STA $%02X", 3, 1),
+    STXZ(Registers::x, AddressingMode.ZEROPAGE, 0x86, "STX $%02X", 3, 1),
+    STYZ(Registers::y, AddressingMode.ZEROPAGE, 0x84, "STY $%02X", 3, 1),
+
+    // Zeropage indexed
+    STAZX(Registers::a, AddressingMode.ZEROPAGE_INDEXED_X, 0x95, "STA $%02X,X", 4, 1),
+    STXZY(Registers::x, AddressingMode.ZEROPAGE_INDEXED_Y, 0x96, "STX $%02X,Y", 4, 1),
+    STYZX(Registers::y, AddressingMode.ZEROPAGE_INDEXED_X, 0x94, "STY $%02X,X", 4, 1),
+
+    // Indexed indirect
+    STAIX(Registers::a, AddressingMode.INDEXED_INDIRECT, 0x81, "STA ($%02X,X)", 6, 1),
+
+    // Indirect indexed
+    STAIY(Registers::a, AddressingMode.INDIRECT_INDEXED, 0x91, "STA ($%02X,X)", 6, 1);
+
+
 
     private final int opcode;
     private final String mnemonic;
--- src/test/java/com/pjfsw/sixfiveoto/instruction/StaTest.java
+++ src/test/java/com/pjfsw/sixfiveoto/instruction/StaTest.java
@@ -11,7 +11,7 @@ public class StaTest {
     private static final int VALUE = 17;
 
     @Test
-    public void testAbsolute() {
+    public void testStaAbsolute() {
         int addr = 0x200;
         Workbench wb = new Workbench(StMemory.STA.opcode(), Word.lo(addr), Word.hi(addr));
         wb.registers().a(VALUE);
@@ -20,7 +20,25 @@ public class StaTest {
     }
 
     @Test
-    public void testAbsoluteX() {
+    public void testStxAbsolute() {
+        int addr = 0x200;
+        Workbench wb = new Workbench(StMemory.STX.opcode(), Word.lo(addr), Word.hi(addr));
+        wb.registers().x(VALUE);
+        assertEquals(4,wb.run(1));
+        assertEquals(VALUE, wb.peek(addr));
+    }
+
+    @Test
+    public void testStyAbsolute() {
+        int addr = 0x200;
+        Workbench wb = new Workbench(StMemory.STY.opcode(), Word.lo(addr), Word.hi(addr));
+        wb.registers().y(VALUE);
+        assertEquals(4,wb.run(1));
+        assertEquals(VALUE, wb.peek(addr));
+    }
+
+    @Test
+    public void testStaAbsoluteX() {
         int offset = 2;
         int addr = 0x200;
         Workbench wb = new Workbench(StMemory.STAX.opcode(), Word.lo(addr), Word.hi(addr));
@@ -32,7 +50,7 @@ public class StaTest {
 
 
     @Test
-    public void testAbsoluteY() {
+    public void testStaAbsoluteY() {
         int offset = 2;
         int addr = 0x200;
         Workbench wb = new Workbench(StMemory.STAY.opcode(), Word.lo(addr), Word.hi(addr));
@@ -40,6 +58,71 @@ public class StaTest {
         wb.registers().y(offset);
         assertEquals(5,wb.run(1));
         assertEquals(VALUE, wb.peek(addr + offset));
+    }
+
+    @Test
+    public void testStaZeroPage() {
+        int addr = 0x10;
+        Workbench wb = new Workbench(StMemory.STAZ.opcode(), Word.lo(addr));
+        wb.registers().a(VALUE);
+        assertEquals(3,wb.run(1));
+        assertEquals(VALUE, wb.peek(addr));
+    }
+    @Test
+    public void testStxZeroPage() {
+        int addr = 0x10;
+        Workbench wb = new Workbench(StMemory.STXZ.opcode(), Word.lo(addr));
+        wb.registers().x(VALUE);
+        assertEquals(3,wb.run(1));
+        assertEquals(VALUE, wb.peek(addr));
+    }
+    @Test
+    public void testStyZeroPage() {
+        int addr = 0x10;
+        Workbench wb = new Workbench(StMemory.STYZ.opcode(), Word.lo(addr));
+        wb.registers().y(VALUE);
+        assertEquals(3, wb.run(1));
+        assertEquals(VALUE, wb.peek(addr));
+    }
+
+    @Test
+    public void testStaZeroPageIndexed() {
+        int addr = 0x10;
+        int offset = 2;
+        Workbench wb = new Workbench(StMemory.STAZX.opcode(), Word.lo(addr));
+        wb.registers().a(VALUE);
+        wb.registers().x(2);
+        assertEquals(4, wb.run(1));
+        assertEquals(VALUE, wb.peek(addr+offset));
+    }
+
+    @Test
+    public void testIndexedIndirect() {
+        int zp = 0x10;
+        int addr = 0x200;
+        int offset = 2;
+        Workbench wb = new Workbench(StMemory.STAIX.opcode(), Word.lo(zp));
+        wb.poke(zp+offset, Word.lo(addr));
+        wb.poke(zp+offset+1, Word.hi(addr));
+        wb.registers().a(VALUE);
+        wb.registers().x(offset);
+        assertEquals(6, wb.run(1));
+        assertEquals(VALUE, wb.peek(addr));
+    }
+
+    @Test
+    public void testIndirectIndexed() {
+        int zp = 0x10;
+        int addr = 0x200;
+        int offset = 2;
+        Workbench wb = new Workbench(StMemory.STAIY.opcode(), Word.lo(zp));
+        wb.poke(zp, Word.lo(addr));
+        wb.poke(zp+1,Word.hi(addr));
+
+        wb.registers().a(VALUE);
+        wb.registers().y(offset);
+        assertEquals(6, wb.run(1));
+        assertEquals(VALUE, wb.peek(addr+offset));
     }
 
 }

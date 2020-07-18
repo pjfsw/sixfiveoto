4dd42e28bccf6b042f57c4a5f3778eb35b529f62
Johan Fransson <per.johan.fransson@gmail.com>
2020-07-18 13:52:48 +0200

    Updated arduino code

--- src/main/arduino/gameduino/gameduino.c
+++ src/main/arduino/gameduino/gameduino.c
@@ -4,6 +4,9 @@
 
 #define GDSEL 9
 
+int img = 63;
+int spriteNo = 255;
+
 void gd_write(unsigned int addr) {
   digitalWrite(GDSEL, LOW);
   SPI.transfer(0x80 | (addr >> 8));
@@ -30,8 +33,8 @@ void init_gd() {
   SPSR = (1 << SPI2X);
   digitalWrite(GDSEL, HIGH);
 }
-void setup() {
-  init_gd();
+
+void dump() {
   Serial.begin(115200);
 
   char output[8];
@@ -40,7 +43,7 @@ void setup() {
   int mod = 16;
 
   Serial.println("--BEGIN DATA--");
-  for (int i = 0; i < 0x2900; i++) {
+  for (unsigned int i = 0; i < 0x8000; i++) {
     int v = SPI.transfer(0);
     if (i % mod == 0) {
       Serial.println();
@@ -54,10 +57,101 @@ void setup() {
   Serial.println("--END DATA--");
   Serial.end();
   gd_end();
+}
 
+void sprite(int x, int y) {
+  // Sprite position
+  gd_write(0x3000 + spriteNo*4);
+  SPI.transfer(x & 255);
+  SPI.transfer(0x80 | (x >> 8) | (2*5));
+  SPI.transfer(y & 255);
+  SPI.transfer((y >> 8) | (img << 1));
+  gd_end();
+}
+
+
+void drawSprite() {
+
+  // SPrite image data
+  gd_write(0x4000 + 256 * img);
+  for (int i = 0; i < 240; i++) {
+    SPI.transfer(i % 4);
+  }
+  for (int i = 0; i < 8; i++) {
+    SPI.transfer(1);
+  }
+  for (int i = 0; i < 8; i++) {
+    SPI.transfer(0);
+  }
+
+  gd_end();
+
+  int x = 10;
+  int y = 10;
+  
+  // Sprite palette
+  gd_write(0x2880);
+  SPI.transfer(0);
+  SPI.transfer(0);
+
+  SPI.transfer(0xFF);
+  SPI.transfer(0x7F);
+
+  SPI.transfer(0x1F);
+  SPI.transfer(0x00);
+
+  SPI.transfer(0x0F);
+  SPI.transfer(0x7F);
+  gd_end();
+}
+
+void scroll() {
+  gd_write(0x2804);
+  SPI.transfer(0);
+  SPI.transfer(0);
+  SPI.transfer(0);  
+  gd_end();
+}
+
+void setup() {
+  delay(2000);
+  init_gd();
+  gd_write(0);
+  char *s = "GAMEDUINO ASCII TEST FROM 6502!";
+  do {
+    SPI.transfer(*s);
+    s++;
+  } while (*s != 0);
+  gd_end();
+  gd_write(0x280B);
+  SPI.transfer(0);
+  gd_end();
+  drawSprite();
+  scroll();
 }
 
 void loop() {
+    //sprite(0,i);
+    //delay(16);
+    gd_read(0x2802);
+    int frame = SPI.transfer(0);
+    gd_end();
+    char s[4];
+    sprintf(s, "%02X", frame);
+    gd_write(64);
+    SPI.transfer(s[0]);
+    SPI.transfer(s[1]);
+    gd_end();
+    sprite(frame,frame);
+
+  //delay(2000);
+  //gd_write(0x280A);
+  //SPI.transfer(1);
+  //gd_end();
+  //delay(2000);
+  //gd_write(0x280A);
+  //SPI.transfer(0);
+  //gd_end();
 }
 
 int main(int argc, char **argv) {
--- src/main/arduino/gameduino/readserial.py
+++ src/main/arduino/gameduino/readserial.py
@@ -4,7 +4,7 @@ import serial
 import io
 
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
-sio = io.TextIOWrapper(ser)
+sio = io.TextIOWrapper(ser, encoding="ascii")
 
 while True:
     #print(ser.readline())

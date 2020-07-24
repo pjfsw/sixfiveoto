d53fc96f323af8b97655a680e26d53a879b0eb22
Johan Fransson <per.johan.fransson@gmail.com>
2020-07-24 07:36:16 +0200

    Playing around with Java audio interface

--- src/main/java/com/pjfsw/sixfiveoto/AudioTest.java
+++ src/main/java/com/pjfsw/sixfiveoto/AudioTest.java
@@ -1,9 +1,18 @@
 package com.pjfsw.sixfiveoto;
 
+import java.io.File;
+import java.io.IOException;
+
 import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.Mixer;
+import javax.sound.sampled.Mixer.Info;
 import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class AudioTest {
     private static final int BUFFER_SIZE_SAMPLES = 512;
@@ -41,10 +50,46 @@ public class AudioTest {
             }
         }
     }
+
+    void playClip() throws LineUnavailableException, IOException, InterruptedException,
+        UnsupportedAudioFileException {
+
+        AudioFormat af = new AudioFormat(SAMPLE_RATE, 16, 1, true, false);
+
+        try (
+            AudioInputStream sound1 = AudioSystem.getAudioInputStream(new File("/Users/johanfr/priv/eclipsecpp/pixla/poop2.wav"));
+            Clip clip1 = AudioSystem.getClip();
+            Clip clip2 = AudioSystem.getClip()
+        ) {
+            clip1.open(sound1);
+            int samples = SAMPLE_RATE/880;
+            byte[] beep = new byte[samples*2];
+            for (int i = 0; i < samples; i++) {
+                int v = 0;
+                for (int harmonics = 1; harmonics < 20; harmonics+=2) {
+                    v += (int)(28000.0 * Math.sin(harmonics*2*i*Math.PI/samples) / harmonics);
+                }
+
+                beep[i*2] = (byte)(v & 0xff);
+                beep[i*2+1] = (byte)(v >> 8);
+            }
+
+            clip2.open(af, beep, 0, beep.length);
+            FloatControl volume = (FloatControl)clip2.getControl(FloatControl.Type.MASTER_GAIN);
+            volume.setValue(-6.0f);
+            clip1.start();
+            clip2.loop(SAMPLE_RATE/(10*samples));
+            Thread.sleep(1000);
+            clip2.stop();
+            Thread.sleep(1000);
+            clip1.stop();
+        }
+    }
+
     public static void main(String[] args) {
         try {
-            new AudioTest().play();
-        } catch (LineUnavailableException e) {
+            new AudioTest().playClip();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }

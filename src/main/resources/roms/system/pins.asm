4fc660655d55d22709e260ad870e6392d9370482
Johan Fransson <per.johan.fransson@gmail.com>
2021-07-13 18:38:48 +0200

    Use jofmodore.git bios and pins code (todo fix maven dependency)

--- src/main/resources/roms/system/pins.asm
+++ src/main/resources/roms/system/pins.asm
@@ -1,27 +1,5 @@
 #importonce
-
-.label VIA = $D000
-.label PORTB = VIA
-.label PORTA = VIA+1
-.label DDRB = VIA+2
-.label DDRA = VIA+3
-.label SR = VIA+10
-
-.label SS_PORT = PORTB
-.const GD_SS = 1 << 1
-.const CART_SS = 1 << 2
-
-.label SPI_PORT = PORTA
-.const CLOCK = $01
-.const MOSI = 1 << 6
-.const MISO = 1 << 7
-
-.const A_OUTPUTS = CLOCK | MOSI
-.const B_OUTPUTS = GD_SS | CART_SS
-
-.const GD_SELECT = GD_SS ^ $ff
-.const CART_SELECT = CART_SS ^ $ff
-.const IDLE = $ff
+#import "../../../../../../jofmodore/src/main/6502/pins.asm"
 
 .const JOY_PORT = PORTB
 .const PIN_JOY_UP = 3
